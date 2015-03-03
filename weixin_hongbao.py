c54cc9611b8206848c39fe4d2bd7e789c7e7d1ea
kiven <kiven.mr@gmail.com>
2015-03-03 16:05:19 +0800

    Update weixin_hongbao.py

--- weixin_hongbao.py
+++ weixin_hongbao.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import random
+import math
 __author__ = 'kiven'
 
 '''
@@ -9,16 +10,16 @@ __author__ = 'kiven'
 # 微信红包算法1
 def _weixin_hongbao(money,n):
     # 方案一
-    _res = ['%.2f'%(random.uniform(0,money)) for x in xrange(n)]
-    _rev = [float(x) for x in _res] # 换成浮点数字
-    _sum = sum(_rev) # 求和
-    return ['%.2f'%(x*money/_sum) for x in _rev]
+    _res = [float('%.2f'%(random.uniform(0.01,money))) for x in xrange(n)]
+    _sum = sum(_res) # 求和
+    return [float('%.2f'%(x*money/_sum)) for x in _res]
 
+# 微信红包算法2
 def _weixin_hongbao_(money,n):
-    # 方案二
     pass
 
 
+'''
 # 小伙伴排队领
 persons = ['aaaa','lee','sb','jack']
 hongbao = _weixin_hongbao(10,len(persons))
@@ -26,3 +27,57 @@ result = {} # 领取结果
 for x in xrange(len(persons)):
     result[persons[x]] = hongbao[x]
 print result # 输出结果:{'aaaa': '1.61', 'sb': '2.49', 'lee': '3.02', 'jack': '2.87'}
+'''
+
+
+
+class Weixin_Hongbao(object):
+
+    # 转换成分
+    def __init__(self,money,count):
+        self.money = money*100
+        self.count = count
+        self.max = self.money
+        self.min = 1
+        self.offset = 1
+
+    # 生成红包
+    def generate(self):
+        self.result = []
+        self.average = self.money / self.count
+        for i in range(self.count):
+            if self._next(self.max,self.min) > self.average:
+                self._money = self.min + math.sqrt(random.randint(self.min,(self.average - self.min)**2))
+            else:
+                self._money = self.max - math.sqrt(random.randint(self.min,(self.max - self.average)**2))
+            self._money = round(self._money)
+            self.result.append(self._money)
+            self.money -= self._money
+        # 自我修补
+        self.repair(self.money,self.count,self.max,self.min)
+        # 转换成元
+        return [x/100 for x in self.result]
+
+    '''
+    修补程序
+    '''
+    def repair(self,total,count,max,min):
+        while total > 0:
+            for i in range(count):
+                if total > 0 and self.result[i] < max:
+                    self.result[i] += self.offset;
+                    total -= 1
+        while total < 0:
+            for i in range(count):
+                if total < 0 and self.result[i] > min:
+                    self.result[i] -= self.offset;
+                    total += 1
+
+    def _next(self,max,min):
+        return random.randint(min,max - min)
+
+
+
+wxhb = Weixin_Hongbao(100,10)
+print wxhb.generate()
+
